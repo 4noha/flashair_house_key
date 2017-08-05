109584f660d3aa9ec461667a76e6df1bfcf5346e
nokkii <nokkii.03@gmail.com>
2017-08-05 12:01:38 +0900

    Change digital port for FlashAir Dojinshi 4

--- lock.lua
+++ lock.lua
@@ -1,28 +1,32 @@
+local WAIT_TIME = 100;
+
 local lock = function()
+    local totalwait = 0;
     local ok, data = fa.pio( 0x06, 0x02 );
     if ok then
-        while data ~= ( 0x16 ) do
+        --[[
+            Lua 5.2.1
+            Lock: 10
+            Release: 26
+            Unlock: 18
+            while ( data ~= ( 80 ) ) and totalwait < 5000 do
+        ]]
+        while ( data ~= ( 10 ) ) and totalwait < 5000 do
             sleep( 100 );
+            totalwait = totalwait + WAIT_TIME;
             ok, data = fa.pio( 0x06, 0x02 );
         end
         
         if data then
-            fa.pio( 0x06, 0x16 );
-
-            print [[
-                HTTP/1.1 200 OK
-                Content-Type: application/json
-            ]]
+            fa.pio( 0x06, 0x00 );
+            --print( 'HTTP/1.1 200 OK Content-Type: application/json' );
             print( '{"error":"","data":' .. data .. '}' );
             return;
         end
     end
 
     ok, indata = fa.pio( 0x06, 0x00 );
-    print [[
-        HTTP/1.1 500 OK
-        Content-Type: application/json
-    ]]
+    print( 'HTTP/1.1 500 OK Content-Type: application/json' );
     print( '{"error":"I/O error.","data":' .. data .. '}' );
 end
 
--- unlock.lua
+++ unlock.lua
@@ -1,28 +1,32 @@
+WAIT_TIME = 100;
+
 local unlock = function()
+    local totalwait = 0;
     local ok, data = fa.pio( 0x06, 0x04 );
     if ok then
-        while data ~= ( 0x08 ) do
+        --[[
+            Lua 5.2.1
+            Lock: 12
+            Release: 28
+            Unlock: 20
+            while ( data ~= ( 80 ) ) and totalwait < 5000 do
+        ]]
+        while ( data ~= ( 20 ) ) and totalwait < 5000 do
             sleep( 100 );
+            totalwait = totalwait + WAIT_TIME;
             ok, data = fa.pio( 0x06, 0x04 );
         end
         
         if data then
             fa.pio( 0x06, 0x00 );
-            
-            print [[
-                HTTP/1.1 200 OK
-                Content-Type: application/json
-            ]]
+            --print( 'HTTP/1.1 200 OK Content-Type: application/json' );
             print( '{"error":"","data":' .. data .. '}' );
             return;
         end
     end
 
     ok, indata = fa.pio( 0x06, 0x00 );
-    print [[
-        HTTP/1.1 500 OK
-        Content-Type: application/json
-    ]]
+    print( 'HTTP/1.1 500 OK Content-Type: application/json' );
     print( '{"error":"I/O error.","data":' .. data .. '}' );
 end
 
